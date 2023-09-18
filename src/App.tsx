0d93c645c5348214a494f4f4dcc42ee42faa5f37
Vanshika <vanshika06065@gmail.com>
2023-09-18 22:41:56 +0530

    added selection box

--- src/App.tsx
+++ src/App.tsx
@@ -11,28 +11,23 @@ function App() {
   };
   const [editorTheme, setEditorTheme] = useState<string>("vs");
 
-  const handleThemeChange = (theme: string) => {
-    setEditorTheme(theme);
+  const handleThemeChange: React.ChangeEventHandler<HTMLSelectElement> = (
+    e
+  ) => {
+    setEditorTheme(e.target.value);
+    // console.log(`theme ${e.target.value}`);
   };
 
   return (
     <>
       <h2>Select a theme:</h2>
-      <div className="flex justify-start ml-8 items-start">
+      <select onChange={handleThemeChange} value={editorTheme}>
         {themes.map((theme) => (
-          <label key={theme}>
-            <input
-              type="radio"
-              name="theme"
-              value={theme}
-              checked={editorTheme === theme}
-              onChange={() => handleThemeChange(theme)}
-              className="mr-2 ml-1"
-            />
+          <option key={theme} value={theme}>
             {theme}
-          </label>
+          </option>
         ))}
-      </div>
+      </select>
 
       <div className="border-solid border-2 border-indigo-600 m-12">
         <Editor
