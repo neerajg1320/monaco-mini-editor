fa194de1cca3b3758ac642c49ff856dcc05831a9
Vanshika <vanshika06065@gmail.com>
2023-09-18 21:25:22 +0530

    added theme radio buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import {  useState } from 'react';
-import './App.css'
-import Editor from '@monaco-editor/react';
-// import 
+import { useState } from "react";
+import "./App.css";
+import Editor from "@monaco-editor/react";
+// import
 
 function App() {
-
-  const [text, setText] = useState('');
-  const handleText: React.ChangeEventHandler<HTMLTextAreaElement> = (e)=>{
+  const [text, setText] = useState("");
+  const themes = ["vs", "vs-dark", "hc-black", "hc-light"];
+  const handleText: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     setText(e.target.value);
-  }
+  };
+  const [editorTheme, setEditorTheme] = useState<string>("vs");
+
+  const handleThemeChange = (theme: string) => {
+    setEditorTheme(theme);
+  };
 
   return (
     <>
-    <div className='border-solid border-2 border-indigo-600 m-12'>
+      <h2>Select a theme:</h2>
+      <div className="flex justify-start ml-8 items-start">
+        {themes.map((theme) => (
+          <label key={theme}>
+            <input
+              type="radio"
+              name="theme"
+              value={theme}
+              checked={editorTheme === theme}
+              onChange={() => handleThemeChange(theme)}
+              className="mr-2 ml-1"
+            />
+            {theme}
+          </label>
+        ))}
+      </div>
 
-      <Editor height="90vh" defaultLanguage="javascript" defaultValue="// some comment" theme="vs-dark" />
-    </div>
-      <textarea value={text} onChange={handleText}/>
+      <div className="border-solid border-2 border-indigo-600 m-12">
+        <Editor
+          height="90vh"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+          theme={editorTheme}
+        />
+      </div>
+      <textarea value={text} onChange={handleText} />
     </>
-  )
+  );
 }
 
 export default App;
