b1e487d68373d144201710e5f179274604e9d8c0
Vanshika <vanshika06065@gmail.com>
2023-09-18 22:44:51 +0530

    added border on selection box

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
   return (
     <>
       <h2>Select a theme:</h2>
+      <div className="border-2 inline-block border-gray-700 ml-12">
       <select onChange={handleThemeChange} value={editorTheme}>
         {themes.map((theme) => (
           <option key={theme} value={theme}>
@@ -28,6 +29,8 @@ function App() {
           </option>
         ))}
       </select>
+      </div>
+      
 
       <div className="border-solid border-2 border-indigo-600 m-12">
         <Editor
