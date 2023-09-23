f1602b286beb3ba755a69aaa7e37ede7fd38135d
Vanshika <vanshika06065@gmail.com>
2023-09-23 16:12:55 +0530

    Responsive navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import { useTheme } from '../../contexts/Theme.context';
-import './Navbar.style.scss'
+import React, { useState } from "react";
+import { useTheme } from "../../contexts/Theme.context";
+import "./Navbar.style.scss";
 
 const Navbar = () => {
-  const {theme} = useTheme();
-  return (
-    <div className='flex justify-between pl-14 h-[12vh] items-center nav-background' style={{...theme as React.CSSProperties}}>
-      WebAppStarter
+  const { theme } = useTheme();
+  const [toggle, setToggle] = useState(false);
 
-      <ul className='flex w-1/2 justify-around'>
-        <li>HOME</li>
-        <li>Contact</li>
-        <li>About</li>
+  const handleToggle = ()=>{
+    setToggle(!toggle);
+  }
+
+  return (
+    <div
+      className="flex justify-between h-[12vh] items-center nav-background"
+      style={{ ...(theme as React.CSSProperties) }}
+    >
+      <div className="pl-14">
+        <i className="fa fa-bars text-2xl sm:hidden cursor-pointer mr-5" onClick={handleToggle}></i>
+        WebAppStarter
+      </div>
+
+      <ul className={toggle?`flex w-1/2 flex-col top-0 absolute  h-screen  justify-start items-center pl-5 sm:justify-around sm:flex-row sm:relative sm:h-auto sm:pl-0 pt-16 sm:pt-0 sm:left-0 sidebar-animation    sm:bg-transparent sm:shadow-none background-sm`:'hidden sm:flex sm:justify-around sm:flex-row  sm:relative sm:h-auto sm:pl-0 pt-16 sm:pt-0 w-1/2 background-lg'}>
+        <li className="cursor-pointer sm:hidden absolute top-5 right-5" onClick={handleToggle}>&#10006;</li>
+        <li className="py-4 sm:py-0">HOME</li>
+        <li className="py-4 sm:py-0">Contact</li>
+        <li className="py-4 sm:py-0">About</li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
