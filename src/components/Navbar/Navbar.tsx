5cecf6318eb876090d796b1a517450ee71e4da84
Vanshika <vanshika06065@gmail.com>
2023-09-23 19:40:46 +0530

    toggle completed animation remaining

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -20,8 +20,8 @@ const Navbar = () => {
         WebAppStarter
       </div>
 
-      <ul className={toggle?`flex w-1/2 flex-col top-0 absolute  h-screen  justify-start items-center pl-5 sm:justify-around sm:flex-row sm:relative sm:h-auto sm:pl-0 pt-16 sm:pt-0 sm:left-0 sidebar-animation    sm:bg-transparent sm:shadow-none background-sm`:'hidden sm:flex sm:justify-around sm:flex-row  sm:relative sm:h-auto sm:pl-0 pt-16 sm:pt-0 w-1/2 background-lg'}>
-        <li className="cursor-pointer sm:hidden absolute top-5 right-5" onClick={handleToggle}>&#10006;</li>
+      <ul className={`sidebar ${toggle? 'sidebar': 'none'}`}>
+        <li className="cursor-pointer sm:hidden absolute top-5 right-5 text-2xl" onClick={handleToggle}><i className="fa fa-close" ></i></li>
         <li className="py-4 sm:py-0">HOME</li>
         <li className="py-4 sm:py-0">Contact</li>
         <li className="py-4 sm:py-0">About</li>
